78686cc7d9eeb9d98161abd173ded429051f3789
Marcus ParkerGarruba <mparkergarruba@gmail.com>
2022-08-11 12:41:38 -0400

    commit tree e41c77ce47fa3342b82952c09ce5f59dcc5a55b5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find_by!(id: params[:user_id])
+      items = user.items
+    else 
+      items = Item.all
+    end
     render json: items, include: :user
   end
+
+  def show
+    item = Item.find_by!(id: params[:id])
+    render json: item, include: :user
+  end
+
+  def create 
+    item = Item.create!(item_params)
+    render json: item, include: :user, status: :created
+  end
+
+  private
+    def item_params
+      params.permit(:name, :description, :price, :user_id)
+    end
+
+    def render_not_found(error)
+      render json: { message: error.message}, status: :not_found
+    end
 
 end
